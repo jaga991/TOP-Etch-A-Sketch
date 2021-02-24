6cc40ffe4aef763a0825a40fdcca2a9cf5ee034d
jaga991 <66861775+jaga991@users.noreply.github.com>
2021-02-24 14:35:05 +0800

    added display input and output logic

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,18 +1,46 @@
-//Operations
+/* Set up initial query selectors */
 
-function add(a, b){
-    return (a + b);
+//display
+let displayOutput = document.getElementById('display-output');
+let displayInput = document.getElementById('display-input');
+
+//button
+let inputBtn = document.querySelectorAll('[data-input]');
+let operatorBtn = document.querySelectorAll('[data-operator]');
+let clearBtn = document.querySelector('[data-clear]');
+let equalBtn = document.querySelector('[data-equal]');
+
+function main() {
+    let innerCalculation;
+    let inputBtnArr = [...inputBtn];
+    let operatorBtnArr = [...operatorBtn];
+
+    for(let i = 0; i < inputBtnArr.length; i++) {
+        inputBtnArr[i].addEventListener('click', () => {
+            console.log("input clicked");
+            if(displayInput.innerText.length < 20){
+                displayInput.innerText += inputBtnArr[i].getAttribute('data-input');
+            }
+        })
+    }
+
+    for(let i = 0; i < operatorBtnArr.length; i++){
+        operatorBtnArr[i].addEventListener('click', () => {
+            if(operatorBtnArr[i].getAttribute('data-operator') != "pos-neg"){
+                displayOutput.innerText += " " + displayInput.innerText + " " + operatorBtn[i].getAttribute('data-operator');
+                displayInput.innerText = "";
+            }
+            else if(operatorBtnArr[i].getAttribute('data-operator') == "pos-neg"){
+                displayInput.innerText *= -1;
+            }
+        })
+    }
 }
 
-function subtract(a, b){
-    return (a - b);
-}
+main();
 
-function multiply(a, b){
-    return (a * b);
-}
 
-function divide(a, b){
-    return (a / b);
-}
-
+//if input btn clicked, append number value to innerCalculation and display input;
+//if operator btn clicked, append operator to innerCalcutation and displayInput, append displayInput value to displayOutput, clear displayInput
+//if clear btn clicked, clear innerCalculation, displayInput, and displayOutput
+//if equal btn clicked, if no error, append innerCalculate result in displayInput
