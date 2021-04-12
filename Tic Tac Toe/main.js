4984a466688ba6fc73cd5499ed17b84b406169c4
jaga991 <66861775+jaga991@users.noreply.github.com>
2021-04-12 12:25:23 +0800

    a

---
+++ Tic Tac Toe/main.js
@@ -0,0 +1,15 @@
+"use strict";
+/*Internally keeps track of game status*/
+const gameboard = (() => {
+    let gameStatus = new Array(9);
+})();
+
+/*html display logics handled here*/
+const displayController = (() => {
+
+})();
+
+const player = () => {
+    
+}
+
